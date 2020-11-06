1f949ea1c208ac83ee0e76e50f58e7b4b6e42916
Gettam97 <71600033+Gettam97@users.noreply.github.com>
2020-11-06 22:01:17 +0100

    Projectiles

---
+++ Assets/Script/Plantspawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plantspawn : MonoBehaviour
+{
+    float ShootCooldown;
+    public GameObject projectile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootCooldown += Time.deltaTime;
+
+        if(ShootCooldown >= 1)
+        {
+            GameObject projectilexy = GameObject.Instantiate(projectile);
+            projectilexy.transform.position = transform.position;
+            ShootCooldown = 0;
+            
+        }
+    }
+}
---
+++ Assets/Script/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public float MovementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      GameObject monster = GameObject.Find("LilMonster");
+        //transform.position = Vector2.MoveTowards(transform.position, monster.transform.position, 0.03f);
+        Vector2 direction = monster.transform.position - transform.position;
+        direction = direction.normalized * MovementSpeed * Time.fixedDeltaTime;
+        rigidbody.MovePosition((Vector2)transform.position + direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Destroy(this.gameObject);
+    
+    }
+}
