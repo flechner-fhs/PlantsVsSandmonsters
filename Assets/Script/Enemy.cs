b54b92f1bf98fba77fb20aab27d4d7feffe39f22
flechner-fhs <72152373+flechner-fhs@users.noreply.github.com>
2020-11-06 20:44:54 +0100

    Added Enemy Class

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Entity
+{
+    public override void Move()
+    {
+        //Attack
+    }
+
+}
