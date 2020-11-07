5a14b6b9ead8e16f8e87a2cdf3efbd48c724e95d
Noobesch <jonas.kubesch@gmail.com>
2020-11-07 03:46:48 +0100

    Basic Structure for Menu, Options and Title Screen

---
+++ Assets/Script/ToggleMusik.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleMusik : MonoBehaviour
+{
+    AudioSource backgroundMusic;
+    Toggle m_Toggle;
+    GameObject thisToggle;
+
+    public void ToggleMusikMethod(bool isMusicOn)
+    {
+        m_Toggle = GetComponent<Toggle>();
+        
+        if (backgroundMusic == null)
+        {
+            thisToggle = GameObject.Find("MusikToggle");
+            backgroundMusic = GetComponent<AudioSource>();
+            backgroundMusic.loop = true;
+        }
+
+
+        if (m_Toggle.isOn == true)
+        {
+            backgroundMusic.Play();
+        }
+        else
+        {
+            backgroundMusic.Stop();
+        }
+    }
+}
