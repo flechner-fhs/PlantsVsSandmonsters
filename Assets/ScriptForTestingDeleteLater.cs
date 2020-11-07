6da350c2ab03d3faf7f84b582406c18d2f4acea7
Noobesch <jonas.kubesch@gmail.com>
2020-11-07 14:31:39 +0100

    Changed Options and Menu Scene to Activatable Objects

---
+++ Assets/ScriptForTestingDeleteLater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptForTestingDeleteLater : MonoBehaviour
+{
+    public float frames = 0;
+    void Update()
+    {
+        frames += Time.deltaTime;
+        if (frames > 2)
+        {
+            this.transform.Rotate(new Vector3(20, 20, 20));
+            frames = 0;
+        }
+    }
+}
