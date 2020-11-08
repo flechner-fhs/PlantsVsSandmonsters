f91ec5b81fa21a05f5bb493891713111f248b7b1
Gettam97 <71600033+Gettam97@users.noreply.github.com>
2020-11-08 15:24:19 +0100

    NEW Projectiles

---
+++ Assets/Script/Projectiles/SnipyProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnipyProjectile : PlantProjectile
+{
+    bool first = true;
+
+    public new void Fly()
+    {
+        if (first)
+        {
+            first = false;
+            TargetFinding tf = new TargetFinding();
+            Vector2 direction = Vector2.zero;
+            if (tf.findSpecialTarget(ref direction, target, transform.position))
+            {
+                Rigidbody.MovePosition(direction.normalized * MovementSpeed * Time.fixedDeltaTime * AttRange);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (lifeTime > 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
