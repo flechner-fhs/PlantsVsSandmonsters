827a738ad52b53274f0decfedbf76f81464a54ce
Gettam97 <71600033+Gettam97@users.noreply.github.com>
2020-11-09 00:35:28 +0100

    FIXES

--- Assets/Script/Entities/Plant.cs
+++ Assets/Script/Entities/Plant.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Plant : Entity
 {
     public float ShootCooldown;
@@ -26,6 +27,7 @@ public class Plant : Entity
     {
         base.Start();
         transform.position += SpawnOffset;
+        DrawRange((float)AttRange);
     }
 
     public override void Move()
@@ -74,5 +76,27 @@ public class Plant : Entity
     public override void Die()
     {
         Destroy(gameObject);
+    }
+
+    void DrawRange(float radius)
+    {
+        int vertexNumber = 50;
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.loop = true;
+        float angle = 2 * Mathf.PI / vertexNumber;
+        lineRenderer.positionCount = vertexNumber;
+
+        for (int i = 0; i < vertexNumber; i++)
+        {
+            Matrix4x4 rotationMatrix = new Matrix4x4(new Vector4(Mathf.Cos(angle * i), Mathf.Sin(angle * i), 0, 0),
+                                                     new Vector4(-1 * Mathf.Sin(angle * i), Mathf.Cos(angle * i), 0, 0),
+                                       new Vector4(0, 0, 1, 0),
+                                       new Vector4(0, 0, 0, 1));
+            Vector3 initialRelativePosition = new Vector3(0, radius, 0);
+            lineRenderer.SetPosition(i, transform.position + rotationMatrix.MultiplyPoint(initialRelativePosition));
+
+        }
     }
 }
