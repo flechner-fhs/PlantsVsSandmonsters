f8e9f392ae5c2c5b02f7d25cab53842964e008c0
Gettam97 <71600033+Gettam97@users.noreply.github.com>
2020-11-08 16:52:23 +0100

    dsa

--- Assets/Script/Entities/PlantBomba.cs
+++ Assets/Script/Entities/PlantBomba.cs
@@ -1,12 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class PlantBomba : Plant
 {
+    private List<GameObject> characterInRangeList = new List<GameObject>();
+
     public override void Die()
     {
         base.Die();
+        try
+        {
+            GameObject[] monsters = GameObject.FindGameObjectsWithTag("Enemy");
+            characterInRangeList = monsters.Where(x => ((Vector3)transform.position - x.transform.position).magnitude < AttRange).ToList();
+            foreach (GameObject monster in monsters)
+            {
+                monster.GetComponent<Enemy>().TakeDamage(2);
+            }
+        }
+        catch (Exception e)
+        {
 
+        }
     }
 }
